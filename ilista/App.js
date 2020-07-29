b5ef1ec1fe971ef688e872651639c4d503b87fd6
otetLopez <otet.tud@gmail.com>
2020-07-28 23:19:43 -0400

    Porting Initial View

--- ilista/App.js
+++ ilista/App.js
@@ -5,7 +5,18 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <div className="app">
+        <h1>ILISTA</h1>
+        <p><i>A companion.  A simple note application</i></p>
+        <br/><br/><br/><br/><br/>
+        <div className="todo-list">
+          Inser Note Display here
+        </div>
+        <br/><br/><br/><br/><br/> <br/><br/>
+        <div>
+          <button class="smallButtons" onClick={() => alert("Test")}>+</button>
+        </div>
+      </div>
       <StatusBar style="auto" />
     </View>
   );
