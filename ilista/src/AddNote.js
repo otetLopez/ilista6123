ee13163b9ecb625170a1819e689770110555cbf4
otetLopez <otet.tud@gmail.com>
2020-07-30 10:22:11 -0400

    Making passing arguments work in mobile

--- ilista/src/AddNote.js
+++ ilista/src/AddNote.js
@@ -5,7 +5,15 @@ import 'react-native-gesture-handler';
 import bg from '../assets/bg.jpg';
 export const INFO_LOG = "INFO_DEBUG: ";
 
-function NoteForm() {
+'use strict';
+
+
+/**
+ * Name: createNewNote() 
+ * This function routes user to add new note view.  Called when "Add New Note" button is called.
+ * The note_index is just incremented when there is a note being pushed
+ */
+export function createNewNote({navigation}) {
     const [title_in, setTitle] = React.useState("");
     const [content_in, setContent] = React.useState("");
 
@@ -40,54 +48,35 @@ function NoteForm() {
         fontSize: 15,
         marginTop: '1%',
     };
-
-    return (
-
-        <View style={style_noteform}>
-            <TextInput
-                placeholder="New Note"
-                id="title_in"
-                value={title_in}
-                style={style_title_in}
-                onChangeText={text => setTitle(text)}
-                onChange= {e => titleState = e.target.value}
-            />
-            <TextInput 
-                id="content_in"
-                placeholder="Enter you note details here..."
-                value={content_in}
-                onChangeText={text => setContent(text)}
-                style={style_content_in}
-                onChange= {e => {contentState = e.target.value; console.log(contentState);}}
-                multiline={true}
-            />
-        </View>
-    );
-}
-
-var titleState = "";
-var contentState = "";
-/**
- * Name: createNewNote() 
- * This function routes user to add new note view.  Called when "Add New Note" button is called.
- * The note_index is just incremented when there is a note being pushed
- */
-export function createNewNote({navigation}) {
     return (
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
         <ImageBackground source={bg} style={styles.bg}>
             <View style={{height: '100%', width: '100%', alignItems: 'center', padding: 30}}>
                 <Text style= {{fontSize: 40, fontWeight: 'bold', color: '#f0f0f0'}}>ILISTA</Text>
                 <Text style= {{color: '#f0f0f0', fontStyle: 'italic'}}>{"\n"}A companion.  A simple note application </Text>
-                <NoteForm/>
+                <View style={style_noteform}>
+                    <TextInput
+                        placeholder="New Note"
+                        id="title_in"
+                        value={title_in}
+                        style={style_title_in}
+                        onChangeText={text => setTitle(text)}
+                    />
+                    <TextInput 
+                        id="content_in"
+                        placeholder="Enter you note details here..."
+                        value={content_in}
+                        onChangeText={text => setContent(text)}
+                        style={style_content_in}
+                        multiline={true}
+                    />
+                </View>
                 <View style={{marginTop: '10%'}}> 
                     <Button title='Save' onPress={() => {
-                        var title = titleState;
-                        var content = contentState;
-                        console.log(INFO_LOG + "Before: " + title + content);
+                        console.log(INFO_LOG + "Before: " + title_in + content_in);
                         navigation.navigate('Home', {
-                            newTitle: title,
-                            newContent: content,
+                            newTitle: title_in,
+                            newContent: content_in,
                         });
                     }}/>
                     <Button title='Cancel' onPress={() => navigation.goBack()}/> 
